7264d516fb522e46d0bb6296d09301bb8364bae3
Dmitri Mendis <2dmendis@gmail.com>
2018-06-05 04:55:08 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -21,6 +21,12 @@ class ChattyStudent < Student
     puts "Pick me!"
     super 
     puts "Pick me!"
+    super 
+    puts "Pick me!"
+    super 
+    puts "Pick me!"
+    super 
+    puts "Pick me!"
     
   end 
 end 
